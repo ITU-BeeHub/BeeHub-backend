108489903ece68bcfe18780c5f572dec7d4ef98d
dervisgenc <gencd20@itu.edu.tr>
2024-07-15 17:53:43 +0300

    swagger-ui implemented

--- BeeHub/cmd/beeHub/main.go
+++ BeeHub/cmd/beeHub/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// @title BeeHub Ders Seçim Botu API
+// @version 1.0
+// @description Bu, BeeHub Ders Seçim Botu için API dokümantasyonudur.
+
+// @host localhost:8080
+// @BasePath /
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	// Swagger handler
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	// Serve the swagger.yaml file
+	r.GET("/swagger.yaml", func(c *gin.Context) {
+		c.File("swagger.yaml")
+	})
+
+	// Example route
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	r.Run(":8080")
 }
